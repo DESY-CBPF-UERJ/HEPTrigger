ce7f3e3b5a376ae794a7b255d4128ae7130ed5d2
Mataias.C <mataiascost@gmail.com>
2022-08-16 09:35:40 -0300

    Incluido a TAG 'NMu2' no draw_ScaleFactors_systematics.C

--- MACROS/Macro_Eff_trig.py
+++ MACROS/Macro_Eff_trig.py
@@ -65,21 +65,24 @@ if (type==17 or type == 'all'):
 
 
 if (type == 18 or type == 'all'):
-    os.system("root -q Conversion_Data_A_2018.C")
-    os.system("root -q Conversion_Data_B_2018.C")
-    os.system("root -q Conversion_Data_C_2018.C")
-    os.system("root -q Conversion_Data_D_2018.C")
-    os.system("root -q Conversion_ttbar_2018.C")
-    os.system("hadd -f ../datasets/2018/_Run2018Full.root ../datasets/2018/_Run2018A.root ../datasets/2018/_Run2018B.root ../datasets/2018/_Run2018C.root ../datasets/2018/_Run2018D.root")
-    os.system("hadd -f ../datasets/2018/_MC2018.root ../datasets/2018/_ttbar.root")
+    # os.system("root -q Conversion_Data_A_2018.C")
+    # os.system("root -q Conversion_Data_B_2018.C")
+    # os.system("root -q Conversion_Data_C_2018.C")
+    # os.system("root -q Conversion_Data_D_2018.C")
+    # os.system("root -q Conversion_ttbar_2018.C")
+    # os.system("hadd -f ../datasets/2018/_Run2018Full.root ../datasets/2018/_Run2018A.root ../datasets/2018/_Run2018B.root ../datasets/2018/_Run2018C.root ../datasets/2018/_Run2018D.root")
+    # os.system("hadd -f ../datasets/2018/_MC2018.root ../datasets/2018/_ttbar.root")
 
 
 
-    os.system("root -q '../trigEffStudy.C(\"/home/matheus/Desktop/tt-triggerEfficiency-DL/TriggerFiles_2018/\", \"false\",\"/home/matheus/Desktop/tt-triggerEfficiency-DL/datasets/2018/_Run2018Full.root\", \"true\", 2018)'")
-    os.system("root -q '../trigEffStudy.C(\"/home/matheus/Desktop/tt-triggerEfficiency-DL/TriggerFiles_2018/MCPT/\", \"true\",\"/home/matheus/Desktop/tt-triggerEfficiency-DL/datasets/2018/_MC2018.root\", \"true\", 2018)'")
+    # os.system("root -q '../trigEffStudy.C(\"/home/matheus/Desktop/tt-triggerEfficiency-DL/TriggerFiles_2018/\", \"false\",\"/home/matheus/Desktop/tt-triggerEfficiency-DL/datasets/2018/_Run2018Full.root\", \"true\", 2018)'")
+    # os.system("root -q '../trigEffStudy.C(\"/home/matheus/Desktop/tt-triggerEfficiency-DL/TriggerFiles_2018/MCPT/\", \"true\",\"/home/matheus/Desktop/tt-triggerEfficiency-DL/datasets/2018/_MC2018.root\", \"true\", 2018)'")
 
     # os.system("root -q 'draw_ScaleFactors_systematics.C(\"2018\",\"/home/matheus/Desktop/tt-triggerEfficiency-DL/TriggerFiles_2018/data/outfile_Run2018Full.root\" ,\"/home/matheus/Desktop/tt-triggerEfficiency-DL/TriggerFiles_2018/MCPT/MC/outfile_MC2018.root\",\"\",\"/home/matheus/Desktop/tt-triggerEfficiency-DL/outputs_and_plotting_script/2018/\",1,\"\")'")
 
+    list_varied_SF = ["_NMu2"]
+    for x in list_varied_SF:
+        os.system("root -q 'draw_ScaleFactors_systematics.C(\"2018\",\"/home/matheus/Desktop/tt-triggerEfficiency-DL/TriggerFiles_2018/data/outfile_Run2018Full.root\" ,\"/home/matheus/Desktop/tt-triggerEfficiency-DL/TriggerFiles_2018/MCPT/MC/outfile_MC2018.root\",\"\",\"/home/matheus/Desktop/tt-triggerEfficiency-DL/outputs_and_plotting_script/2018/\",1,\""+x+"\")'")
 
     # list_varied_SF = ["_NJETSHIGH","_NJETSLOW","_NPVHIGH","_NPVLOW","_METHIGH","_METLOW"]
     # for x in list_varied_SF:
@@ -93,7 +96,7 @@ if (type == 18 or type == 'all'):
     #     os.system("root -q 'draw_ScaleFactors_systematics.C(\"2018\",\"/home/matheus/Desktop/tt-triggerEfficiency-DL/TriggerFiles_2018/data/outfile_Run2018"+x+".root\" ,\"/home/matheus/Desktop/tt-triggerEfficiency-DL/TriggerFiles_2018/MCPT/MC/outfile_MC2018.root\",\""+x+"\",\"/home/matheus/Desktop/tt-triggerEfficiency-DL/outputs_and_plotting_script/2018/\",0,\"\")'")
 
     # os.system("root -q 'ScaleFactors_syst_lumi.C()'")
-    os.system("root -q '../systCombiner.C(\"/home/matheus/Desktop/tt-triggerEfficiency-DL/outputs_and_plotting_script/2018\")'")
+    # os.system("root -q '../systCombiner.C(\"/home/matheus/Desktop/tt-triggerEfficiency-DL/outputs_and_plotting_script/2018\")'")
 
 
 
